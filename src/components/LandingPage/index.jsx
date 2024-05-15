d84399d65fc91945f86c54d60182a56fe1ae6aae
Fabio Flauzino <fabio@mfrural.com.br>
2024-05-15 09:55:56 -0300

    finalizado responsivo

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -3,22 +3,31 @@ import styles from './LandingPage.module.css'
 import Logo from '../../../public/barbearia-logo.png'
 import Lua from '../../../public/moon.png'
 import Sol from '../../../public/sun.png'
+import { useState } from 'react'
 
 export default function LandingPage() {
+  const dark = "Dark";
+  const light = "Light";
+  const [mode, setMode] = useState(true);
+
+  function trocarTema() {
+    setMode(!mode)
+  }
+
   return (
-    <section className={`${styles.landpage_container} ligth-mode`}>
+    <section className={`${styles.landpage_container} ${mode ? "light-mode" : "dark-mode"}`}>
       <header>
         <div className={styles.header}>
-          <Image src={Logo} className={styles.logo}/>
-          <button>
-            <Image src={Lua} className={styles.icon}/> Dark
+          <Image alt='Imagem' src={Logo} className={styles.logo}/>
+          <button onClick={trocarTema}>
+            <Image alt='Imagem' src={mode ? Lua : Sol} className={styles.icon}/> {mode ? dark : light}
           </button>
         </div>
       </header>
       <section className={styles.banner}></section>
       <section className={styles.body}>
         <h1>Bem-vindo a Barber Shop</h1>
-        <p>
+        <p className={styles.bold}>
           Nossa barbearia sempre oferece profissionais de qualidade e
           estamos prontos para lidar com suas maiores expectativas.
         </p>
@@ -27,7 +36,7 @@ export default function LandingPage() {
           de mestres barbeiros em vários concursos de estilo.
           Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.
         </p>
-        <p>S. Kelly</p>
+        <p className={styles.fantasy}>S. Kelly</p>
       </section>
     </section>
   )
